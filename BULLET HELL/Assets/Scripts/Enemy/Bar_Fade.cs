aa1a1479f18199814031b5183e820fa6fdd593a3
Alvin-Tulud <alvintulud@gmail.com>
2023-10-20 15:46:57 -0700

    bar fade stuff prolly fix healthbar too

--- BULLET HELL/Assets/Scripts/Enemy/Bar_Fade.cs
+++ BULLET HELL/Assets/Scripts/Enemy/Bar_Fade.cs
@@ -14,16 +14,48 @@ public class Bar_Fade : MonoBehaviour
     private Color color_inline;
     private Color color_fill;
 
+    private float dim;
+    private const float dim_amount = 0.1f;
+
+    private int stay;
+    private const int temp_stay = 50;
+
     void Start()
     {
         color_outline = outline.GetComponent<Color>();
         color_inline = inline.GetComponent<Color>();
         color_fill = fill.GetComponent<Color>();
+
+        color_outline.a = 0;
+        color_inline.a = 0;
+        color_fill.a = 0;
+
+        stay = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (stay >= 0)
+        {
+            stay--;
+        }
+        else if (dim != 0)
+        {
+            color_outline.a = dim;
+            color_inline.a = dim;
+            color_fill.a = dim;
+            dim -= dim_amount;
+        }
+    }
+
+    public void fade()
+    {
+        dim = 1;
+        stay = temp_stay;
+
+        color_outline.a = dim;
+            color_inline.a = dim;
+            color_fill.a = dim;
     }
 }
--- BULLET HELL/Assets/Scripts/Enemy/Enemy_Hit.cs
+++ BULLET HELL/Assets/Scripts/Enemy/Enemy_Hit.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Enemy_Hit : MonoBehaviour
 {
     private HealthBar healthBar;
+    private Bar_Fade Bar_Fade;
 
     void Start()
     {
         healthBar = this.gameObject.GetComponentInChildren<HealthBar>();
+        Bar_Fade = this.gameObject.GetComponentInChildren<Bar_Fade>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            takeDamage(1);
+        }
     }
 
     public void takeDamage(int damage)
     {
         healthBar.setHealth(healthBar.getHealth() - damage);// add healthbar fade in fade out
+        Debug.Log("took " + damage + " hp");
+        Bar_Fade.fade();
     }
 }
