d4203b385fe4eb21a25c044c73bb69caea2eb581
unknown <lalvarado.rivas3134@gmail.com>
2023-10-21 15:54:45 -0700

    Added NPC visual trigger

---
+++ BULLET HELL/Assets/Scripts/NPC/DialogueTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+    //only activates dialogue prompt UI when player is nearby
+
+    [Header("TriggerVisual")]
+    [SerializeField] private GameObject triggerVisual;
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+    private bool playerInRange;
+    private void Awake() {
+
+ 
+        playerInRange = false;
+        triggerVisual.SetActive(false);
+    }
+    private void Update() {
+        if (playerInRange) {
+            triggerVisual.SetActive(true);
+            if (Input.GetKeyDown("e")){
+                Debug.Log("TEST PRINT :DDDD");
+            }
+
+        }
+        else {
+            triggerVisual.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerInRange = true;
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerInRange = false;
+        }
+
+    }
+
+}
