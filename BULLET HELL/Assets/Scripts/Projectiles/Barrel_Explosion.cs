e51fcabe6b69a708a59245f491e3954bf06165e9
Alvin-Tulud <alvintulud@gmail.com>
2023-11-08 01:20:58 -0800

    barrel boom and die

--- BULLET HELL/Assets/Scripts/Projectiles/Barrel_Explosion.cs
+++ BULLET HELL/Assets/Scripts/Projectiles/Barrel_Explosion.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class Barrel_Explosion : MonoBehaviour
 {
     public LayerMask layerBoom;
     public float boomDistance;
+
+    private byte countdown;
+    private Color color;
+    private byte r;
+    private byte g;
+    private byte b;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponentInChildren<CircleCollider2D>().radius = 0f;
         GetComponentInChildren<Image>().enabled = false;
+
+        countdown = 255;
+        color = GetComponentInChildren<Image>().color;
+        r = (byte)(color.r * 255);
+        g = (byte)(color.g * 255);
+        b = (byte)(color.b * 255);
+    }
+
+    private void Update()
+    {
+        if (GetComponentInChildren<Image>().enabled)
+        {
+            GetComponentInChildren<Image>().color = new Color32(r, b, g, countdown);
+            countdown -= 10;
+        }
+
+        if (countdown == (byte)0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void boom()
