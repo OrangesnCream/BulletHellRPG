aa9e24f1c737519af5974fe49dc18ff7ec07e4ad
Alvin-Tulud <alvintulud@gmail.com>
2023-11-29 00:46:55 -0800

    dumb color bar broke for 2+ stacks

--- BULLET HELL/Assets/Scripts/Enemy/Enemy_HealthBar.cs
+++ BULLET HELL/Assets/Scripts/Enemy/Enemy_HealthBar.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class Enemy_HealthBar : MonoBehaviour
 {
     public Slider slider;
-    public Image image;
+    public Image healthColor;
+    public Image inlineColor;
     public int maxHealth;
     private int currentHealth;
     public int stacks;
@@ -22,23 +23,32 @@ public class Enemy_HealthBar : MonoBehaviour
         currentHealth = maxHealth;
         this.slider.maxValue = maxHealth / stacks;
         this.slider.value = maxHealth / stacks;
+        this.gradient.mode = GradientMode.Fixed;
 
         ColorKeys = new GradientColorKey[stacks];
         AlphaKeys = new GradientAlphaKey[stacks];
         for (int i = 0; i < stacks && stacks > 1; i++)
         {
-            byte red = (byte)((255 / stacks) * (i + 1));
+            byte red = (byte)((255 / stacks) * (stacks - i));
             ColorKeys[i] = new GradientColorKey (new Color32(red, 0, 0, 255), ((float)i + 1) / (float)stacks);
-            AlphaKeys[i] = new GradientAlphaKey(1.0f, ((float)i + 1) / (float)stacks);
+            AlphaKeys[i] = new GradientAlphaKey (1.0f, ((float)i + 1) / (float)stacks);
         }
 
         if (stacks == 1)
         {
-            ColorKeys[0] = new GradientColorKey(new Color32(255, 0, 0, 255), 1.0f);
-            AlphaKeys[0] = new GradientAlphaKey(1.0f, 1.0f);
+            ColorKeys[0] = new GradientColorKey (new Color32(255, 0, 0, 255), 1.0f);
+            AlphaKeys[0] = new GradientAlphaKey (1.0f, 1.0f);
         }
+        Array.Reverse(ColorKeys);
+        Array.Reverse(AlphaKeys);
 
         this.gradient.SetKeys(ColorKeys, AlphaKeys);
+
+        this.healthColor.color = this.gradient.Evaluate(1.0f);
+        if (currentStack == 1)
+            this.inlineColor.color = Color.white;
+        else
+            this.inlineColor.color = this.gradient.Evaluate(((float)currentStack - 1) / (float)stacks);
     }
 
     public void damage(int damage)
@@ -57,7 +67,13 @@ public class Enemy_HealthBar : MonoBehaviour
             this.slider.value -= damage;
         }
 
-        this.image.color = gradient.Evaluate((float)currentStack / stacks); // get % and cast to float
+        this.healthColor.color = this.gradient.Evaluate ((float)currentStack / (float)stacks); // get % and cast to float
+
+        if (currentStack == 1)
+            this.inlineColor.color = Color.white;
+        else
+            this.inlineColor.color = this.gradient.Evaluate(((float)currentStack - 1) / (float)stacks);
+        Debug.Log((float)currentStack / (float)stacks);
     }
 
     public int getMaxHealth() { return this.maxHealth; }
