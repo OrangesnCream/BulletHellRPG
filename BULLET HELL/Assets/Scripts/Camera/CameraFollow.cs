51cef73137bee1fc811f38ee10cab222a8146079
LuisAlvaradoRivas <lalvaradorivas@sfsu.edu>
2024-04-02 20:31:22 -0700

    again

--- BULLET HELL/Assets/Scripts/Camera/CameraFollow.cs
+++ BULLET HELL/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,43 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class CameraFollow : MonoBehaviour
 {
+    public Transform player;
     private Vector3 offset = new Vector3(0, 0, -10);
-    private float smoothTime = 0.3f;
+    Vector3 target, mousePos, refVel, shakeOffset;
+    float cameraDist = 5.0f;
+    float smoothTime = 0.2f, zStart;
     private Vector3 velocity = Vector3.zero;
 
     private bool isToggled = true;
 
     public bool isBossCamera = false;
-
-    [SerializeField] private Transform target;
+    void Start()
+    {
+        target=player.position;
+        zStart = transform.position.z;
+    }
 
     void Update(){
-        if(Input.GetKeyDown(KeyCode.C)){
-            isToggled = !isToggled;
-        }
-
-        if(Input.GetKeyDown(KeyCode.B)){
-            Debug.Log("Toggling boss camera");
-            isBossCamera = !isBossCamera;
-        }
-
-        if(isBossCamera){
-            target = GameObject.FindGameObjectWithTag("BossCamera").transform;
-            transform.GetComponent<Camera>().orthographicSize = Mathf.Lerp(transform.GetComponent<Camera>().orthographicSize, 20, 0.01f);
-        }
-        else{
-            target = GameObject.FindGameObjectWithTag("Player").transform;
-            transform.GetComponent<Camera>().orthographicSize = Mathf.Lerp(transform.GetComponent<Camera>().orthographicSize, 10, 0.001f);
-        }
+        
     }
 
     void FixedUpdate(){
-        if(isToggled){
-            Vector3 targetPosition = target.TransformPoint(offset);
+        if(isBossCamera){
+            Vector3 targetPosition = player.TransformPoint(offset);
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
+        mousePos = CaptureMousePos();
+        if(isBossCamera){
+            player = GameObject.FindGameObjectWithTag("BossCamera").transform;
+            transform.GetComponent<Camera>().orthographicSize = Mathf.Lerp(transform.GetComponent<Camera>().orthographicSize, 20, 0.01f);
+        }
+        else{
+            target = UpdateTargetPos();
+            UpdateCameraPosition();
+        }
+    }
+    Vector3 CaptureMousePos() {
+        Vector2 ret = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        ret *= 2;
+        ret -= Vector2.one;
+        float max = 0.9f;
+        if (Mathf.Abs(ret.x)>max||Mathf.Abs(ret.y)>max) {
+            ret = ret.normalized;
+        }
+        return ret;
+    }
+    Vector3 UpdateTargetPos()
+    {
+        Vector3 mouseOffset = mousePos * cameraDist;
+        Vector3 ret = player.position + mouseOffset;
+        ret.z = zStart;
+        return ret;
+    }
+    void UpdateCameraPosition()
+    {
+        Vector3 tempPos;
+        tempPos=Vector3.SmoothDamp(transform.position,target,ref refVel,smoothTime);
+        transform.position=tempPos;
     }
 }
