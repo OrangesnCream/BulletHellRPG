a618fed3bfe5b15a826b66315719a29351c77072
LuisAlvaradoRivas <lalvaradorivas@sfsu.edu>
2024-05-13 22:41:00 -0700

    created combat section for level 1

--- BULLET HELL/Assets/Scripts/Player/PlayerController.cs
+++ BULLET HELL/Assets/Scripts/Player/PlayerController.cs
@@ -165,46 +165,47 @@ public class PlayerController : MonoBehaviour
 
     void Move(){
         if (CurrentStairs.Count == 0){
-        rb.velocity = movement.normalized * speed;
-        }
-        Stairs stairs = CurrentStairs.Peek();
-        Vector2 stairsDirection = stairs.GetDirection();
+            rb.velocity = movement.normalized * speed;
+        }else{
+            Stairs stairs = CurrentStairs.Peek();
+            Vector2 stairsDirection = stairs.GetDirection();
 
-        // apply slows for vertical direction
-        movement.y *= (Mathf.Sign(stairsDirection.y) == Mathf.Sign(movement.y)) ? StairSlowDownYNeg : StairSlowDownYPos;
-        float originalLength = movement.magnitude;
+            // apply slows for vertical direction
+            movement.y *= (Mathf.Sign(stairsDirection.y) == Mathf.Sign(movement.y)) ? StairSlowDownYNeg : StairSlowDownYPos;
+            float originalLength = movement.magnitude;
 
-        float angle = stairs.Angle;
-        bool isVertical = angle == 0;
+            float angle = stairs.Angle;
+            bool isVertical = angle == 0;
 
-        // since we are using the range 0-180, we need to do some clean up in the angle here
-        // I'm sure there is a cleaner way to do this, but it works so whatever.
-        bool isRight = angle > 90;
-        if (isRight) {
-        angle = angle - 90;
-        } else {
-        angle = 90 - angle;
-        }
-        // calculate tan, negate based on the angle because of math
-        float tan = -Mathf.Tan(angle * Mathf.Deg2Rad);
-        if (isRight) {
-        tan *= -1;
-        }
-        // For vertical stairs we need to override this to 0 since it will increase y infinitely when our angle is 0
-        if (isVertical)
-        tan = 0;
+            // since we are using the range 0-180, we need to do some clean up in the angle here
+            // I'm sure there is a cleaner way to do this, but it works so whatever.
+            bool isRight = angle > 90;
+            if (isRight) {
+            angle = angle - 90;
+            } else {
+            angle = 90 - angle;
+            }
+            // calculate tan, negate based on the angle because of math
+            float tan = -Mathf.Tan(angle * Mathf.Deg2Rad);
+            if (isRight) {
+            tan *= -1;
+            }
+            // For vertical stairs we need to override this to 0 since it will increase y infinitely when our angle is 0
+            if (isVertical)
+            tan = 0;
 
-        // SPECIFIC CASE: Player walks diagonally down stairs
-        // This results in the player not moving in the y direction (cancels out due to tan angle)
-        // we allow them to move a bit because even though its "logically" correct, it feels restrictive.
-        // This is a perfect example of not following exact realism for the sake of game-feel
-        if (Mathf.Sign(stairsDirection.x) != Mathf.Sign(movement.x) && movement.y > 0) {
-        tan /= 2;
+            // SPECIFIC CASE: Player walks diagonally down stairs
+            // This results in the player not moving in the y direction (cancels out due to tan angle)
+            // we allow them to move a bit because even though its "logically" correct, it feels restrictive.
+            // This is a perfect example of not following exact realism for the sake of game-feel
+            if (Mathf.Sign(stairsDirection.x) != Mathf.Sign(movement.x) && movement.y > 0) {
+            tan /= 2;
+            }
+            // apply vector calc to y and normalize to maintain speed
+            movement.y += movement.x * tan;
+            movement = movement.normalized * originalLength;
+            rb.velocity=movement*speed;
         }
-        // apply vector calc to y and normalize to maintain speed
-        movement.y += movement.x * tan;
-        movement = movement.normalized * originalLength;
-        rb.velocity=movement*speed;
     }
 
     private IEnumerator Dash(){
